ebec83826178bda638537a52c17d27c59ef70f2b
Gumle <gumle678+github@gmail.com>
2024-08-19 13:26:13 +0200

    Added password confirmation to RegisterForm

--- bp-projsub.client/src/components/forms/LoginForm.tsx
+++ bp-projsub.client/src/components/forms/LoginForm.tsx
@@ -17,7 +17,7 @@ const formSchema = z.object({
   email: z.string().email({
     message: "Invalid email"
   }),
-  password: z.string().min(8, { message: "Password must be at least 8 characters long" }),
+  password: z.string().min(1, { message: "Invalid password" }),
 })
 
 
--- bp-projsub.client/src/components/forms/RegisterForm.tsx
+++ bp-projsub.client/src/components/forms/RegisterForm.tsx
@@ -20,9 +20,8 @@ const formSchema = z.object({
     message: "Invalid email"
   }),
   password: z.string().min(8, { message: "Password must be at least 8 characters long" }),
-  confirmPassword: z.string().min(8, { message: "Password must be at least 8 characters long"
-  }),
-}).refine((data) => data.password === data.confirmPassword, { message: "Passwords do not match" })
+  confirmPassword: z.string(),
+}).refine((data) => data.password === data.confirmPassword, { message: "Passwords do not match", path: ["confirmPassword"] })
 
 
 export default function RegisterForm() {
