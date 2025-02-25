c26500c0926eaae68d273fd913e13eef2f1e14f5
Gumle <gumle678+github@gmail.com>
2025-02-25 16:53:56 +0100

    Added: CI/CD environments do not have APPDATA or HOME set

--- bp-projsub.client/vite.config.ts
+++ bp-projsub.client/vite.config.ts
@@ -12,6 +12,11 @@ const baseFolder =
         ? `${env.APPDATA}/ASP.NET/https`
         : `${env.HOME}/.aspnet/https`;
 
+// Added: CI/CD environments do not have APPDATA or HOME set
+if (!fs.existsSync(baseFolder)) {
+    fs.mkdirSync(baseFolder, { recursive: true });
+}
+
 const certificateName = "bp-projsub.client";
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
